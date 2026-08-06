63c721f80637f3046576157e70e8c182b77ad780
HARSHAL KARANJE <karanjeharshal007@gmail.com>
2026-08-06 16:14:45 +0530

    Time: 0 ms (100%), Space: 23.9 MB (40.51%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        // there can only be 2 elements n/3 more than repeating
+        int num1 = 0;
+        int num2 = 1;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            } else if (count1 == 0) {
+                num1 = nums[i];
+                count1++;
+            } else if (count2 == 0) {
+                num2 = nums[i];
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == num1) {
+                count1++;
+            } else if (nums[i] == num2) {
+                count2++;
+            }
+        }
+
+        // we have tracked all  num1 and num2 all elements
+        vector<int> ans;
+        int n = nums.size();
+
+        if (count1 > n / 3) {
+            ans.push_back(num1);
+        } 
+        if (count2 > n / 3) {
+            ans.push_back(num2);
+        }
+
+        return ans;
+    }
+};
+
